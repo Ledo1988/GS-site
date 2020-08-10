67e1e120e4c82ca49e46a9c9b8a3a68adf3a7549
Elena <elena.do1988@yandex.ru>
2020-08-10 13:07:02 +0300

    modal.js is redone

--- local/js/modal/modal.js
+++ local/js/modal/modal.js
@@ -1,15 +1,17 @@
+const bodyPage = document.querySelector('.page');
 const modalTriggers = document.querySelectorAll(".modal__open-link");
 const modalCloseTrigger = document.querySelectorAll(".modal__close-link");
-const bodyPage = document.querySelector('.page');
-let modalContent = document.querySelector(".modal__content");
+
 let modalActiveClass = "modal_open";
+let modalContentClass = "modal__content";
+let modalCloseClass = "modal__close-link";
 let bodyOverflowClass = "overflow-hidden";
 let popupModal;
 
 function modalTriggerCheck(trigger) {
     const { popupTrigger } = trigger.dataset;
-    popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`);
 
+    popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`);
     popupModal.classList.toggle(modalActiveClass);
     bodyPage.classList.toggle(bodyOverflowClass);
 
@@ -18,11 +20,9 @@ function modalTriggerCheck(trigger) {
 
 function bodyPageCheck(event) {
 
-    if (bodyPage.classList.contains(bodyOverflowClass) && event.currentTarget !== modalContent) {
+    if (bodyPage.classList.contains(bodyOverflowClass) &&
+        (event.target.closest('.' + modalCloseClass) || !event.target.closest('.' + modalContentClass))) {
 
-        console.log(bodyPage.classList);
-        console.log(event.currentTarget);
-        console.log(popupModal);
         popupModal.classList.remove(modalActiveClass);
         bodyPage.classList.remove(bodyOverflowClass);
     }
@@ -32,6 +32,6 @@ modalTriggers.forEach(trigger => {
     trigger.addEventListener('mouseover', () => { modalTriggerCheck(trigger) }) });
 
 modalCloseTrigger.forEach(trigger => {
-   trigger.addEventListener('mouseover', () => { modalTriggerCheck(trigger) }) });
+   trigger.addEventListener('click', (event) => { bodyPageCheck(event) }) });
 
 bodyPage.addEventListener('click', (event) => { bodyPageCheck(event) });
