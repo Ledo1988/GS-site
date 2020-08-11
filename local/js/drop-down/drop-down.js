87c2feab2a41b2c60f904726a9dcd745def909ab
Elena <elena.do1988@yandex.ru>
2020-08-11 21:57:52 +0300

    configurator with drop-down are added

---
+++ local/js/drop-down/drop-down.js
@@ -0,0 +1,57 @@
+//get elements
+const dropdownTitle = document.querySelector('.drop-down__result');
+const dropdownOptions = document.querySelectorAll('.drop-down__option');
+
+//bind listeners to these elements
+dropdownTitle.addEventListener('click', toggleDropDownMenuDisplay);
+dropdownOptions.forEach(option => option.addEventListener('click', handleDropDownOptionSelected));
+
+function toggleDropDownClass(elem,className){
+    if (elem.className.indexOf(className) !== -1){
+        elem.className = elem.className.replace(className,'');
+    }
+    else{
+        elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
+    }
+
+    return elem;
+}
+
+function toggleDropDownDisplay(elem){
+    const curDisplayStyle = elem.style.display;
+
+    if (curDisplayStyle === 'none' || curDisplayStyle === ''){
+        elem.style.display = 'block';
+    }
+    else{
+        elem.style.display = 'none';
+    }
+}
+
+
+function toggleDropDownMenuDisplay(e){
+    const dropdown = e.currentTarget.parentNode;
+    const menu = dropdown.querySelector('.drop-down__list');
+    const icon = dropdown.querySelector('.drop-down__result-icon');
+
+    toggleDropDownClass(menu,'drop-down__list_hide');
+    toggleDropDownClass(icon,'drop-down__result-icon_rotate');
+}
+
+function handleDropDownOptionSelected(e){
+    toggleDropDownClass(e.target.parentNode, 'drop-down__list_hide');
+
+    const id = e.target.id;
+    const newValue = e.target.textContent + ' ';
+    const titleElem = document.querySelector('.drop-down__result');
+    const icon = document.querySelector('.drop-down__result-icon');
+
+
+    titleElem.textContent = newValue;
+    titleElem.appendChild(icon);
+
+    //trigger custom event
+    document.querySelector('.drop-down__result').dispatchEvent(new Event('change'));
+    //setTimeout is used so transition is properly shown
+    setTimeout(() => toggleDropDownClass(icon,'drop-down__result-icon_rotate',0));
+}
