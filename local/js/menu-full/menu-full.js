47af7c355987c17a8c4e0ebb55f5c92d03fec059
Elena <elena.do1988@yandex.ru>
2020-08-12 18:20:55 +0300

    modal for menu-full is rearranged

--- local/js/menu-full/menu-full.js
+++ local/js/menu-full/menu-full.js
@@ -3,6 +3,7 @@ const menuFullReturnLink = document.querySelector(".menu-full__action-link_retur
 const menuFullCloseLink = document.querySelector(".menu-full__action-link_close");
 const menuFullModal = document.querySelector('.menu-full.modal');
 
+
 menuFullLink.forEach(item => {
     item.addEventListener('click', (event) => { menuFullLinkCheck(event) }) });
 
@@ -11,32 +12,41 @@ menuFullReturnLink.addEventListener('click', () => { menuFullReturnAction() });
 menuFullCloseLink.addEventListener('click', () => { menuFullCloseAction() });
 
 function menuFullLinkCheck(event) {
+
     const eventTarget = event.target.closest('.menu-full__link');
     const eventParent = event.target.closest('.menu-full__item_extra');
     const eventModal = event.target.closest('.modal');
+    let eventParentOpened;
+
+    eventParentOpened =  eventParent.closest('.menu-full__list').querySelector(".menu-full__item_opened");
+    console.log(11);
 
     if (!eventParent
-        || (eventTarget.classList.contains('opened') && eventParent.classList.contains('menu-full__item_opened'))
-        || eventModal.classList.contains('modal_col-2')) {
-
+        || eventParent.classList.contains('menu-full__item_opened'))  {
+        console.log(5);
         return true;
 
-    } else if (eventTarget.classList.contains('opened') && !eventParent.classList.contains('menu-full__item_opened')) {
-
+    }  else if (eventParentOpened) {
+        event.preventDefault();
+        eventParentOpened.classList.remove('menu-full__item_opened');
+        eventParent.classList.add('menu-full__item_opened');
+        console.log(1);
     } else if (eventModal.classList.contains('modal_col-1')) {
         event.preventDefault();
-        eventTarget.classList.add('opened');
         eventParent.classList.add('menu-full__item_opened');
         eventTarget.closest('.modal_col-1').classList.add('modal_col-2');
         eventTarget.closest('.modal_col-1').classList.remove('modal_col-1');
+        console.log(2);
     } else if (eventModal.classList.contains('modal')) {
         event.preventDefault();
-        eventTarget.classList.add('opened');
+
         eventParent.classList.add('menu-full__item_opened');
         eventTarget.closest('.modal').classList.add('modal_overflow-visible');
         eventTarget.closest('.modal').classList.add('modal_col-1');
+        console.log(3);
     } else {
         return true;
+        console.log(6);
     }
 }
 
@@ -54,7 +64,6 @@ function menuFullReturnAction() {
         menuFullModal.classList.remove('modal_overflow-visible')
     }
 
-    console.log(menuExtraOpenedLast);
     menuExtraOpenedLast.querySelector(".menu-full__link").classList.remove('opened');
     menuExtraOpenedLast.classList.remove('menu-full__item_opened');
 
