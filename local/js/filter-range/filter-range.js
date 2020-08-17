1ef64731ac914249690da871106bd849e16a884e
Elena <elena.do1988@yandex.ru>
2020-08-17 22:16:27 +0300

    filter-range is finalized

--- local/js/filter-range/filter-range.js
+++ local/js/filter-range/filter-range.js
@@ -1,4 +1,5 @@
 const filterTriggers = document.querySelectorAll(".filter-range");
+const filterInputAll = document.querySelectorAll(".filter-range__input");
 
 function filterTriggerCheck(filter) {
 
@@ -11,17 +12,18 @@ function filterTriggerCheck(filter) {
     let filterInputNumberMin = currentFilter.querySelector(".filter-range__input_min");
     let filterInputNumberMax = currentFilter.querySelector(".filter-range__input_max");
 
+    let filterReset = currentFilter.querySelector(".filter__btn-reset");
 
     if (filter.dataset.filter === "filter-range_rate") {
         noUISliderMin = 0;
         noUISliderMax = 150000;
-        noUISliderStart = 1000;
-        noUISliderEnd = 90000;
+        noUISliderStart = 0;
+        noUISliderEnd = 150000;
     } else if (filter.dataset.filter === "filter-range_option") {
         noUISliderMin = 0;
         noUISliderMax = 45000;
-        noUISliderStart = 5000;
-        noUISliderEnd = 20000;
+        noUISliderStart = 0;
+        noUISliderEnd = 45000;
     }
 
     noUiSlider.create(currentFilter, {
@@ -38,11 +40,24 @@ function filterTriggerCheck(filter) {
         let value = values[handle];
 
         if (handle) {
-            filterInputNumberMax.value = value;
+            filterInputNumberMax.value = Math.round(value);
+
         } else {
             filterInputNumberMin.value = Math.round(value);
         }
+
+
+        if (filterInputNumberMin.value.replace(" ","") == noUISliderMin && filterInputNumberMax.value.replace(" ","") == noUISliderMax) {
+            currentFilter.classList.remove('filter-range_changed');
+        } else {
+            currentFilter.classList.add('filter-range_changed');
+        }
+
+
+        filterInputAllWidth(filterInputNumberMax);
+        filterInputAllWidth(filterInputNumberMin);
     });
+
 
     filterInputNumberMin.addEventListener('change', function () {
         currentFilter.noUiSlider.set([this.value, null]);
@@ -51,10 +66,32 @@ function filterTriggerCheck(filter) {
     filterInputNumberMax.addEventListener('change', function () {
         currentFilter.noUiSlider.set([null, this.value]);
     });
+
+
+
+    if (filterReset != null) {
+        filterReset.addEventListener('click', function () {
+            currentFilter.noUiSlider.reset()
+        });
+    }
+
+}
+
+function filterInputAllWidth(item) {
+    item.value = item.value.replace(/ /g,"");
+    item.value = item.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+
+    item.style.width = ((item.value.length + 2) * 8) + 'px';
 }
 
 
 filterTriggers.forEach(filter => { filterTriggerCheck(filter) });
+
+filterInputAll.forEach(item => { filterInputAllWidth(item) });
+
+filterInputAll.forEach(item => {
+    item.addEventListener('input', () => { filterInputAllWidth(item) }) });
+
 
 
 
