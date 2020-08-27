8903fff8f962c2619accd9218357cf64a03e5513
Elena <elena.do1988@yandex.ru>
2020-08-27 21:50:21 +0300

    catalog-detailed config is added

---
+++ local/js/slider-config/slider-config.js
@@ -0,0 +1,9 @@
+var sliderConfig = new Swiper('.slider-config', {
+    slidesPerView: 'auto',
+    //spaceBetween: 10,
+    navigation: {
+        nextEl: '.slider-config__button_next',
+        prevEl: '.slider-config__button_prev',
+    },
+})
+
